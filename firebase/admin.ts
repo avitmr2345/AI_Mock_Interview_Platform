58178002bbf917f12b9112e2e7e24281de737d9c
Avish <avitmr2345@gmail.com>
2025-09-01 11:19:10 +0530

    feat: added Firebase Admin and Client SDK integration
    - Added firebase and firebase-admin dependencies.
    - Implemented Firebase Admin SDK & Firebase Client SDK.
    - Configured Firebase initialization with environment variables by creating new private key of project for secure credential management.

---
+++ firebase/admin.ts
@@ -0,0 +1,28 @@
+import { cert, getApps, initializeApp } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
+
+// Initialize Firebase Admin SDK which offers secure server-side operations
+// It allows us to perform mutations on server side and it is more secure as we are making calls from the server. In the 
+// client SDK, the API keys are exposed to the client side which is not secure and have more limited permissions than Admin.
+const initFirebseAdmin = () => {
+  const apps = getApps();
+
+  if (!apps.length) {
+    initializeApp({
+      credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        // Replace newlines in the private key
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      }),
+    });
+  }
+
+  return {
+    auth: getAuth(),
+    db: getFirestore(),
+  };
+};
+
+export const { auth, db } = initFirebseAdmin();
