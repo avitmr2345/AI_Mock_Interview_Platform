5d76057fe8170867b1e342b9a1d422834539a351
Avish <avitmr2345@gmail.com>
2025-09-14 18:00:21 +0530

    feat: added signOut functionality and logged in user name

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,21 +1,33 @@
 import { ReactNode } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { isAuthenticated } from "@/lib/actions/auth.action";
+import { getCurrentUser, isAuthenticated } from "@/lib/actions/auth.action";
 import { redirect } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { signOut } from "@/lib/actions/auth.action";
 
 const RootLayout = async ({ children }: { children: ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated();
 
   if (!isUserAuthenticated) redirect("/sign-in");
 
+  const user = await getCurrentUser();
+
   return (
     <div className="root-layout">
-      <nav>
+      <nav className="flex justify-between">
         <Link href="/" className="flex items-center gap-2">
           <Image src="/logo.svg" alt="Logo" width={38} height={32} />
           <h2 className="text-primary-100">TechAce</h2>
         </Link>
+        <div className="flex items-center gap-6">
+          <p className="capitalize text-xl text-gray-400 font-bold max-sm:gap-4">
+            {user?.name}
+          </p>
+          <Button className="text-[14px]" onClick={signOut}>
+            Sign Out
+          </Button>
+        </div>
       </nav>
 
       {children}
